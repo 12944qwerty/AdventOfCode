13d99df91ec03bf6f7bc455bcfacbdd227f992bf
12944qwerty <qwerty12944qwerty@gmail.com>
2022-12-03 05:40:09 +0000

    optimize
    thanks ken

--- 2022/day03/solution.py
+++ 2022/day03/solution.py
@@ -6,7 +6,7 @@ Part 2: Sum up the "letters" that are common between groups of 3 lines
 """
 
 with open('data.txt', encoding='utf-8') as f:
-    data = f.readlines()
+    data = f.read().splitlines()
 
 ABC = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -15,20 +15,16 @@ for i in data:
     mid = len(i)//2
     a, b = i[:mid], i[mid:]
 
-    for j in set(a):
-        if j in b:
-            total += ABC.index(j)
+    for j in set(a) & set(b):
+        total += ABC.index(j)
 
 print("Part 1:", total)
 
 total = 0
-for i in range(len(data)//3):
-    a = data[i*3].strip()
-    b = data[i*3+1].strip()
-    c = data[i*3+2].strip()
+for i in range(0, len(data), 3):
+    a, b, c = map(set, data[i:i+3])
 
-    for j in set(a):
-        if j in b and j in c:
-            total += ABC.index(j)
+    for j in a & b & c:
+        total += ABC.index(j)
 
 print("Part 2:", total)
