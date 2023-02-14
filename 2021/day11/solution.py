b80df42810727d614fe316dbbc4dd2cc936808ae
12944qwerty <qwerty12944qwerty@gmail.com>
2023-02-14 14:10:40 +0000

    grid implementation

--- 2021/day11/solution.py
+++ 2021/day11/solution.py
@@ -4,18 +4,20 @@ Day 11: Dumbo Octopus
 Part 1: 
 Part 2: 
 """
+from utils import Grid
 
 def parse_data(f):
     data = {}
     for i, line in enumerate(f.read().splitlines()):
         for j, col in enumerate(line):
-            data[(i, j)] = int(col)
+            data[(j, i)] = int(col)
 
-    return data
+    return Grid(data)
 
-def part1(data):
+def part1(data: Grid):
     flashes = 0
     flashed = []
+    check = lambda a, _: 0 < a
     for _ in range(100):
         for i, j in data:
             data[(i, j)] += 1
@@ -26,8 +28,7 @@ def part1(data):
         while flashed:
             x, y = flashed.pop()
             flashes += 1
-            for dx, dy in (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1):
-                c = (x+dx, y+dy)
+            for c in data.neighbors(x, y, diagonals=True, check=check):
                 if data.get(c):
                     data[c] += 1
                     if data[c] > 9:
@@ -39,6 +40,7 @@ def part1(data):
 def part2(data):
     flashed = []
     step = 1
+    check = lambda a, _: 0 < a
     while step > 0:
         for i, j in data:
             data[(i, j)] += 1
@@ -48,8 +50,7 @@ def part2(data):
 
         while flashed:
             x, y = flashed.pop()
-            for dx, dy in (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1):
-                c = (x+dx, y+dy)
+            for c in data.neighbors(x, y, diagonals=True, check=check):
                 if data.get(c):
                     data[c] += 1
                     if data[c] > 9:
