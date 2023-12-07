22637fb73ad49089afe47be16a7e4aed50a1aed3
12944qwerty <qwerty12944qwerty@gmail.com>
2023-12-06 23:32:36 -0500

    update aoc template

--- main.py
+++ main.py
@@ -47,15 +47,29 @@ Day {str(flags.day).zfill(2)}: {puzzle.title}
 Part 1: 
 Part 2: 
 \"\"\"
+import re
+from functools import lru_cache
+from itertools import combinations, permutations, product, combinations_with_replacement
+from collections import Counter, defaultdict, deque
+from math import gcd, lcm, floor, ceil, sqrt, prod
+from utils import grid, number_re
 
 def parse_data(f):
-    return f.read().splitlines()
+    return [a for a in f.read().split("\n")]
 
 def part1(data):
-    pass
+    sum = 0
+    for line in data:
+        sum += 1
+    
+    return sum
 
 def part2(data):
-    pass
+    sum = 0
+    for line in data:
+        sum += 1
+    
+    return sum
 """) # Generate template
 
     try:
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,6 @@ Utils Module to quickly work with common concepts.
 """
 
 from .grid import Grid
+import re
+
+number_re = re.compile(r"\d+")
